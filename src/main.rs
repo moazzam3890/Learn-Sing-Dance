5465f557d8e4a656411f265094a0469aca8d5479
Moazzam Adil Khan <moaadi3890@gmail.com>
2020-05-02 18:16:20 +0500

    multi-threaded implimentation added

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,45 @@
-#[allow(unused_imports)]
+// #[allow(unused_imports)]
 use std::time::Duration;
 use futures::executor::block_on;
-#[allow(unused_imports)]
+// #[allow(unused_imports)]
 use std::thread;
 
 
 fn main() {
+    // calling async main thread and blocking it with the
+    // block_on() executor.
+    println!("IMPLIMENTING THROUGH ASYNC");
     block_on(async_main());
+    // calling threaded function
+    println!("\nIMPLIMENTING THROUGH THREAD");
+    let learning = thread::spawn(||{
+        for i in 0..5{
+            println!("learning {}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    let singing = thread::spawn(||{
+        for i in 0..5 {
+            println!("Singing {}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 0..5 {
+        println!("Dancing {}", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+    learning.join().unwrap();
+    singing.join().unwrap();
 }
 
 async fn async_main() {
-    
+    // calling dancing async function and awaiting it to
+    // return a future
     dance().await;
+    // calling learn_sing async function and awaiting it to
+    // return a future.
     learn_sing().await;
     // let f1 = learn_sing();
     // let f2 = dance();
